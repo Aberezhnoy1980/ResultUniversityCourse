17f8fcc97587753e82ad72f076f01b9663df58b9
Aleksandr Berezhnoy <alex@iMac-IMAC.local>
2025-08-30 20:30:04 +0300

    task part 2 code added

--- js-basis/lesson019_hw/index.js
+++ js-basis/lesson019_hw/index.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+import jsImg from './assets/javaScript.jpeg';
+
+const headerEl = document.createElement('h1');
+headerEl.textContent = 'I love JavaScript'
+document.body.append(headerEl);
+
+const imgEl = document.createElement('img');
+imgEl.src = jsImg;
+document.body.append(imgEl);
--- js-basis/lesson019_hw/rollup.config.mjs
+++ js-basis/lesson019_hw/rollup.config.mjs
@@ -1,4 +1,19 @@
+import styles from "rollup-plugin-styles";
+import image from "@rollup/plugin-image";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
 export default {
 	input: "./index.js",
 	output: { file: "./dist/bundle.js", format: "cjs" },
+	plugins: [
+		styles(),
+		image(),
+		resolve(),
+		babel({ babelHelpers: "bundled" }),
+		serve(),
+		livereload(),
+	],
 };
