c94e595758764be119f27cf40657a17a30f0e61f
Aleksandr Berezhnoy <alex@iMac-IMAC.local>
2025-08-31 13:51:33 +0300

    webpack configured

--- js-basis/lesson019_hw_part_2/webpack.config.js
+++ js-basis/lesson019_hw_part_2/webpack.config.js
@@ -1,11 +1,47 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "index.js"),
-    output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "target"),
-      clean: true,
-    },
+	mode: "development",
+	entry: path.resolve(__dirname, "index.js"),
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "target"),
+		clean: true,
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "index.html"),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						targets: "defaults",
+						presets: [["@babel/preset-env"]],
+					},
+				},
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerPlugin(), "..."],
+	},
 };
