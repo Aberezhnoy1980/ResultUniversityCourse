262887dc539d2bb032d242f71055b1199df11376
Aleksandr <berezhnoy_aa@mail.ru>
2025-09-07 23:08:18 +0300

    code transpiled from jsx to js

--- react/lesson001_react_intro/vite-hw-app/src/App.jsx
+++ react/lesson001_react_intro/vite-hw-app/src/App.jsx
@@ -1,26 +1,59 @@
 import ReactLogo from './assets/react.svg?react';
 import './App.css';
 import Dater from './components/DateComponent';
+import { createElement } from 'react';
 
 // Декларативный стиль
+// export const App = () => {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<ReactLogo />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<Dater />
+// 			</header>
+// 		</div>
+// 	);
+// };
+
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<ReactLogo />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<Dater />
-			</header>
-		</div>
-	);
+	return createElement('div', {
+		className: 'App',
+		children: createElement('header', {
+			className: 'App-header',
+			children: [
+				createElement(ReactLogo, { key: 'logo' }),
+				createElement('p', {
+					key: 'text',
+					children: [
+						'Edit ',
+						createElement('code', {
+							key: 'code',
+							children: 'src/App.js',
+						}),
+						' and save to reload.',
+					],
+				}),
+				createElement('a', {
+					key: 'link',
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+					children: 'Learn React',
+				}),
+				createElement(Dater, { key: 'dater' }),
+			],
+		}),
+	});
 };
--- react/lesson001_react_intro/vite-hw-app/src/components/DateComponent.jsx
+++ react/lesson001_react_intro/vite-hw-app/src/components/DateComponent.jsx
@@ -1,7 +1,19 @@
+// export default function Dater() {
+// 	return (
+// 		<span style={{ marginTop: '1rem', fontWeight: 'bold' }}>
+// 			{new Date().getFullYear()}
+// 		</span>
+// 	);
+// }
+
+import { createElement } from 'react';
+
 export default function Dater() {
-	return (
-		<span style={{ marginTop: '1rem', fontWeight: 'bold' }}>
-			{new Date().getFullYear()}
-		</span>
-	);
+	return createElement('span', {
+		style: {
+			marginTop: '1rem',
+			fontWeight: 'bold',
+		},
+		children: new Date().getFullYear(),
+	});
 }
